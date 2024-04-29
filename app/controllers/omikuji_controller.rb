db3406ffc3ace750a6d19466dcacf2696c300fb6
山本美希 <yamamotomiki@yamamotikinoAir.AirPort>
2024-04-29 14:47:41 +0900

    アイテム更新

--- app/controllers/omikuji_controller.rb
+++ app/controllers/omikuji_controller.rb
@@ -2,18 +2,18 @@ class OmikujiController < ApplicationController
   def show
     outcomes = [
       { luck: "めっちゃラッキー！", item: "本" },
-      { luck: "最高にラッキー！", item: "リップクリーム" },
+      { luck: "最高にラッキー！", item: "靴" },
       { luck: "ちょっとラッキー！", item: "お気に入りのペン" },
       { luck: "スーパーラッキー！", item: "パソコン" },
       { luck: "ラッキー！", item: "イヤホン" },
-      { luck: "まぁまぁラッキー！", item: "お守り" },
-      { luck: "ラッキー！", item: "香水" },
+      { luck: "かなりラッキー！", item: "お守り" },
+      { luck: "ラッキー！", item: "コンビニのおにぎり" },
       { luck: "最高にラッキー！", item: "ティーカップ" },
-      { luck: "とってもラッキー！", item: "現金" },
+      { luck: "ウルトララッキー！", item: "現金" },
       { luck: "まぁまぁラッキー！", item: "腕時計" },
       { luck: "ラッキー！", item: "フライドポテト" },
-      { luck: "最高にラッキー！", item: "エコバッグ" },
-      { luck: "とってもラッキー！", item: "お花"}
+      { luck: "めっちゃラッキー！", item: "エコバッグ" },
+      { luck: "スーパーラッキー！", item: "お花"}
     ]
 
     outcome = outcomes.sample
