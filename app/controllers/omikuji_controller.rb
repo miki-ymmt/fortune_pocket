02c05cdcdb6189e915e2362e5186754ed2868e90
山本美希 <yamamotomiki@yamamotomikinoMacBook-Air.local>
2024-05-01 07:42:29 +0900

    カードサイズ変更

--- app/controllers/omikuji_controller.rb
+++ app/controllers/omikuji_controller.rb
@@ -1,19 +1,19 @@
 class OmikujiController < ApplicationController
   def show #おみくじの結果をランダムで決定
     outcomes = [
-      { luck: "めっちゃラッキー！", item: "本", image: "books.png" },
-      { luck: "最高にラッキー！", item: "靴", image: "shoes.png" },
-      { luck: "ちょっとラッキー！", item: "お気に入りのペン", image: "pen.png" },
-      { luck: "スーパーラッキー！", item: "パソコン", image: "laptop.png" },
-      { luck: "ラッキー！", item: "イヤホン", image: "earphone.png" },
-      { luck: "かなりラッキー！", item: "お守り", image: "luckeycharm.png" },
-      { luck: "ラッキー！", item: "コンビニのおにぎり", image: "onigiri.png" },
-      { luck: "最高にラッキー！", item: "ティーカップ",  image: "teacup.png" },
-      { luck: "ウルトララッキー！", item: "現金", image: "money.png" },
-      { luck: "まぁまぁラッキー！", item: "腕時計", image: "watch.png" },
-      { luck: "ラッキー！", item: "フライドポテト", image: "potatoes.png" },
-      { luck: "めっちゃラッキー！", item: "エコバッグ", image: "ecobag.png" },
-      { luck: "スーパーラッキー！", item: "お花", image: "flowers.png" },
+      { luck: "平穏な一日になるでしょう！", item: "本", image: "books.png" },
+      { luck: "新しいことを始めるのに最適な日！", item: "靴", image: "shoes.png" },
+      { luck: "笑顔が絶えない一日になるでしょう！", item: "お気に入りのペン", image: "pen.png" },
+      { luck: "予期せぬ素敵な出来事が起こるでしょう！", item: "パソコン", image: "laptop.png" },
+      { luck: "新しい趣味に出会えるかも！", item: "イヤホン", image: "earphone.png" },
+      { luck: "いいニュースが聞けるかも！", item: "お守り", image: "luckeycharm.png" },
+      { luck: "潜在能力が開花します！", item: "コンビニのおにぎり", image: "onigiri.png" },
+      { luck: "ゆっくりとした時間を過ごせそう！", item: "ティーカップ",  image: "teacup.png" },
+      { luck: "ポジティブな変化が訪れるでしょう！", item: "現金", image: "money.png" },
+      { luck: "誰かから感謝される日になるでしょう！", item: "腕時計", image: "watch.png" },
+      { luck: "過去の苦労が報われる日です！", item: "フライドポテト", image: "potatoes.png" },
+      { luck: "感動することがあるかも！", item: "エコバッグ", image: "ecobag.png" },
+      { luck: "新しいプロジェクトが成功の予感！", item: "お花", image: "flowers.png" },
     ]
 
     outcome = outcomes.sample
